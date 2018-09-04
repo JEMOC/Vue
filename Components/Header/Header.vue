52a6e9cd7387d68f1beff921ee8361f0c3a00cc2
JEMOC <ccccjiemo@gmail.com>
2018-09-05 00:04:46 +0800

    Header

---
+++ Components/Header/Header.vue
@@ -0,0 +1,217 @@
+<template>
+    <div class="header">
+        <div class="nav">
+            <div class="blur-bg blur" style="background-image: url('http://localhost/30d45ce269948eae8ed650e5ce03a2ca463a5d77.png')"></div>
+            <div class="blur-mask blur"></div>
+            <div class="wrapper clearfix">
+                <div class="nav-content fl">
+                    <ul>
+                        <li class="nav-item">
+                            <a class="home a-style" href="http://www.bilibili.com"><i class="icon-bili"></i>主站</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="a-style">画友</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="a-style">音频</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="a-style">游戏中心</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="a-style">直播</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="a-style">会员购</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="a-style">BML</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="a-style">萌战</a>
+                        </li>
+                        <li class="nav-item nav-icon">
+                            <a class="a-style"><i class="icon-app"></i>下载APP</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="contribution fr">
+                    <a class="upload">投稿</a>
+                </div>
+                <div class="nav-content fr">
+                    <ul>
+                        <li class="nav-item profile">
+                            <a class="a-style">
+                                <div class="img">
+                                    <img class="icon-face" src="http://localhost/akari.jpg" alt="">
+                                </div>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="history a-style">
+                                历史
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="banner" style="background-image:url('http://localhost/30d45ce269948eae8ed650e5ce03a2ca463a5d77.png')">
+            <div class="head-content wrapper">
+                <div class="search">
+
+                    <form class="search-form">
+                        <input type="text" class="keyword" autocomplete="off" placeholder="什么样才叫「渣男界」中的泥石流？">
+                        <input type="submit" value="" class="submit icon-search">
+                    </form>
+                    <a class="rank">
+                        <span>排行榜</span>
+                    </a>
+                </div>
+                <a class="icon-logo">
+                    <img src="http://localhost/6063fb40e90a17912b71d2cfd4ea56f69abb70de.png">
+                </a>
+            </div>
+
+        </div>
+        <div class="wrapper">
+            <div class="menu">
+                <ul class="nav-menu">
+                    <li class="home">
+                        <a class="nav-name">首页
+                            <div class="icon-wrap">
+                                <i class="icon-home"></i>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">动画
+                            <div class="num-wrap">
+                                <span>999+</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">番剧
+                            <div class="num-wrap">
+                                <span>110</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">国创
+                            <div class="num-wrap">
+                                <span>999+</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">音乐
+                            <div class="num-wrap">
+                                <span>999+</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">舞蹈
+                            <div class="num-wrap">
+                                <span>340</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">游戏
+                            <div class="num-wrap">
+                                <span>999+</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">科技
+                            <div class="num-wrap">
+                                <span>999+</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">生活
+                            <div class="num-wrap">
+                                <span>999+</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">鬼畜
+                            <div class="num-wrap">
+                                <span>205</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">时尚
+                            <div class="num-wrap">
+                                <span>784</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">广告
+                            <div class="num-wrap">
+                                <span>172</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">娱乐
+                            <div class="num-wrap">
+                                <span>999+</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">影视
+                            <div class="num-wrap">
+                                <span>999+</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-name">放映厅
+                            <div class="num-wrap">
+                                <span>229</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="side-nav">
+                        <a class="side-item">
+                            <i class="icon-zhuanlan"></i>
+                            <span>专栏</span>
+                        </a>
+                    </li>
+                    <li class="side-nav">
+                        <a class="side-item">
+                            <i class="icon-square"></i>
+                            <span>广场</span>
+                        </a>
+                    </li>
+                    <li class="side-nav">
+                        <a class="side-item">
+                            <i class="icon-live"></i>
+                            <span>直播</span>
+                        </a>
+                    </li>
+                    <li class="side-nav">
+                        <a class="side-item">
+                            <i class="icon-blackroom"></i>
+                            <span>小黑屋</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script src="./Header.js"></script>
+
+<style lang="scss" scoped src="./Header.scss"></style>
